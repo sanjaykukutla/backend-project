d8dd375508917eb454e4a11af208f1ec1fc54719
SanjayKukutla <sanjaykukutla2003@gmail.com>
2024-09-17 18:02:44 +0530

    Updated main folder with new contents

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import express from 'express';
+const app =express();
+const port =process.env.PORT||4000;
+
+//set cors for app and allow only the loac; host 5173
+app.use(cors({origin:'http://localhost:5173'}));
+//app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+//get a list of 5 jokes
+app.get('/api/jokes',(req,res)=>{
+    // each joke has id,title content in json format
+    const jokes =[
+        {id:1,title:'Joke 1',content:'This is joke 1'},
+        {id:2,title:'Joke 2',content:'This is joke 2'},
+        {id:3,title:'Joke 3',content:'This is joke 3'},
+        {id:4,title:'Joke 4',content:'This is joke 4'},
+        {id:5,title:'Joke 5',content:'This is joke 5'}
+    ];
+    res.send(jokes);
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+    //console.log("hi server");
+})
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // add proxy for axios 
+  server:{
+    proxy:{
+      '/api':'http://localhost:4000',
+    },
+  },
+})
